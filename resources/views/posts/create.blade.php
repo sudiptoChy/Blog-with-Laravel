3211ced7bc9d68667046af567d1c358206ee0cec
Sudipto Chowdhury <dip.chy93@gmail.com>
2017-02-09 20:25:02 +0600

    Added Flash Error/Success Messages

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,11 +11,11 @@
           <form method="POST" action="{{ route('posts.store') }}">
             <div class="form-group">
               <label name="title">Title:</label>
-              <input id="title" name="title" class="form-control">
+              <input id="title" name="title" class="form-control" required>
             </div>
             <div class="form-group">
               <label name="body">Post Body:</label>
-              <textarea id="body" name="body" rows="10" class="form-control"></textarea>
+              <textarea id="body" name="body" rows="10" class="form-control" required autofocus=""></textarea>
             </div>
             <input type="submit" value="Create Post" class="btn btn-success btn-lg btn-block">
             <input type="hidden" name="_token" value="{{ Session::token() }}">
