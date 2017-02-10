668b1937796788a0fd66760726cc0d1515eb65ba
Sudipto Chowdhury <dip.chy93@gmail.com>
2017-02-10 19:30:54 +0600

    Edited post view page and added Edit and delete buttons

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,7 +3,39 @@
 @section('title' , '| View Post')
 
 @section('content')
+  <div class="row">
+    <div class="col-md-8">
+      <h1> {{ $post->title }} </h1>
+      <p class="lead"> {{ $post->body }} </p>
+    </div>
 
-  <p class="lead"> This is the blog Post </p>
+    <div class="col-md-4">
+      <div class="well">
+        <dl class="horizontal">
+          <dt>Created at:</dt>
+          <dd>{{ date('M j, Y h:i a', strtotime($post->created_at)) }}</dd>
+        </dl>
+
+        <dl class="horizontal">
+          <dt>Last Updated:</dt>
+          <dd>{{ date('M j, Y h:i a', strtotime($post->updated_at)) }}</dd>
+        </dl>
+        <hr>
+
+        <div class="row">
+          <div class="col-sm-6">
+            <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-primary btn-block">Edit</a>
+          </div>
+
+          <div class="col-sm-6">
+            <a href="{{ route('posts.destroy', $post->id) }}" class="btn btn-danger btn-block">Delete</a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+
 
 @endsection
