1e7db89c883b21e7f3b1bc43398872b687ec2caf
Sudipto Chowdhury Dip <dip.acm77@gmail.com>
2017-05-06 12:52:19 +0600

    Added styling of comment section

--- resources/views/_head.blade.php
+++ resources/views/_head.blade.php
@@ -9,7 +9,7 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
 
     @yield('stylesheet')
   
--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', "|  $post->title ")
 
+@section('stylesheet')
+  <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+@endsection
+
 @section('content')
 
 	<div class="row">
@@ -15,10 +19,21 @@
 
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
+			<h3 class="comments-title"><span class="glyphicon glyphicon-comment"></span> {{ $post->comments()->count() }} Comments</h3>
 			@foreach($post->comments as $comment)
 				<div class="comment">
-					<p><strong>Name:</strong> {{ $comment->name }}</p>
-					<p><strong>Comment:</strong> <br/>{{ $comment->comment }}</p> <br/>
+					<div class="author-info">
+						<img src="{{ "https://www.gravatar.com/avatar/".md5(strtolower(trim($comment->email)))."?s=50&d=mm" }}" class="author-image">
+							<div class="author-name">
+								<h4>{{ $comment->name }}</h4>
+								<p class="author-time">{{ date('F nS, Y -g:iA', strtotime($comment->created_at)) }}</p>
+							</div>
+						
+					</div> 
+
+					<div class="comment-content">
+						{{ $comment->comment }}
+					</div>
 				</div>
 			@endforeach
 		</div>
